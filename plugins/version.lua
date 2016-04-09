b73f56bca9eb6d7bf867d69648ed0c1fd78414a2
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2016-04-09 17:14:42 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,13 +1,13 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v2 \n by BeatBot Team :) <3 \n @BeatBot_Team"
+    return "illusion Self-Bot V1"
 end 
 return {
   patterns = {
     "^#version$",
     "^#bot$",
-    "^#selfbot$"
+    "^#illusion$"
   },
   run = run
 }
